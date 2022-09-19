5d931ddc4436ba9ebc6cc0e36d1126884e7a169e
Techvizi <79307916+Vaishak2312@users.noreply.github.com>
2022-09-19 17:28:52 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <div style ="font-size: x-large" align =" center"> Employee Info</div>
+        <table class="nav-justified">
+            <tr>
+                <td style="width: 278px">&nbsp;</td>
+                <td style="width: 141px">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 278px">&nbsp;</td>
+                <td style="width: 141px">Employee Name</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 278px">&nbsp;</td>
+                <td style="width: 141px">Employee Age</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 278px">&nbsp;</td>
+                <td style="width: 141px">Employee Salary</td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+                    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+                    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+                </td>
+            </tr>
+        </table>
+        <asp:GridView ID="GridView1" runat="server" Width="1377px">
+        </asp:GridView>
+        <br/>
+
+
+
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-3S6LNPA;Initial Catalog=ProgrammingDB;User ID=sa;Password=sa");
+            protected void Button1_Click(object sender, EventArgs e)
+            {
+
+                con.Open();
+                SqlCommand comm = new SqlCommand("Insert into EmployeeInfo_Tab values('" + TextBox2.Text + "','"  +int.Parse(TextBox3.Text) +"','" + double.Parse(TextBox4.Text)+ ,con);
+                comm.ExecuteNonQuery();
+                con.Close();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+                LoadRecord();
+
+            }
+            void LoadRecord()
+            {
+                SqlCommand comm = new SqlCommand("select * from StudentInfo_Tab", con);
+                SqlDataAdapter d = new SqlDataAdapter(comm);
+                DataTable dt = new DataTable();
+                d.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("Update EmployeeInfo_Tab set EmployeeName= '" + TextBox2.Text +"' 
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+            LoadRecord();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("Delete EmployeeInfo_Tab where EmployeeName= '" + TextBox2.Text + "';         
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+            LoadRecord();
+        }
+    }
+}
